f7d8c838c6376f0f733e9b0f76039f0a25561cbb
Andrés Felipe Rodríguez Ramírez <andfrodriguezr@correo.udsitrital.edu.co>
2020-05-01 10:09:25 -0500

    Primer commit, conexion con sql creada, no hay clases establecidas, interfaz basica

---
+++ Biblioteca/Biblioteca/ConexiondbBiblioteca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Biblioteca
+{
+    class ConexiondbBiblioteca
+    {
+        string cadena = "Data source=PC-PC\\TEW_SQLEXPRESS;Initial Catalog=documentos; Integrated Security= True";
+        public SqlConnection conectarbd = new SqlConnection();
+        
+        public ConexiondbBiblioteca()
+        {
+            conectarbd.ConnectionString = cadena;
+        }
+
+        public void abrir()
+        {
+            try
+            {
+                conectarbd.Open();
+                Console.WriteLine("Conexion abierta");
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine("Error al abrir la base de datos"+ex.Message);
+            }
+        }
+
+        public void cerrar()
+        {
+            conectarbd.Close();
+        }
+    }
+
+}
---
+++ Biblioteca/Biblioteca/Menu_principal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biblioteca
+{
+    public partial class Menu_principal : Form
+    {
+        public Menu_principal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Menu_principal_Load(object sender, EventArgs e)
+        {
+            ConexiondbBiblioteca conexion = new ConexiondbBiblioteca();
+            conexion.abrir();
+        }
+    }
+}
